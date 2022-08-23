a9231aab71edfb89ea2677f9bbcc10fcd4aba044
DeepaliPashte <deepalipashte888@gmail.com>
2022-08-23 22:42:25 +0530

    PBA-04 Create Service interface

--- src/main/java/com/Deepali/service/Service.java
+++ src/main/java/com/Deepali/service/Service.java
@@ -1,5 +1,17 @@
 package com.Deepali.service;
 
+import java.util.List;
+
+import com.Deepali.model.Contact;
+
 public interface Service {
+	
+	public boolean saveContact(Contact contact);
+	
+	 public List<Contact>getAllContact();
+	 
+	public  Contact getByContactId(Integer contactId);
+	
+	public boolean updateContact(Contact contact);
 
 }
